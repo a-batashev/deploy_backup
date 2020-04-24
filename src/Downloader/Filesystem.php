0fe4a46deef7237ea2c608494be06a83c7855cfe
Arkharis <arkharis@ya.ru>
2020-04-24 15:31:30 +0300

    Add Downloader\Filesystem

---
+++ src/Downloader/Filesystem.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Downloader;
+
+use App\ArgumentsProcessor;
+use App\Config;
+use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
+use Symfony\Component\Filesystem\Filesystem as FS;
+
+/**
+ * Filesystem
+ */
+class Filesystem implements DownloaderInterface
+{
+    /**
+     * Copy files from backup to sitepath
+     *
+     * @return void
+     */
+    public static function get()
+    {
+        $args = ArgumentsProcessor::getInstance();
+
+        $config = Config::getInstance()->getConfigByPreset();
+
+        $sitePath = $config['sitePath'];
+        $from = $config['transport']['from'];
+
+        if (!file_exists($from)) {
+            throw new \Exception("Directory isn't exist: '{$from}'.");
+        }
+
+        echo "Copy from '{$from}' to '{$sitePath}'", PHP_EOL;
+
+        if (!$args->isDryRun()) {
+            $filesystem = new FS();
+            $filesystem->mirror($from, $sitePath);
+        }
+    }
+}

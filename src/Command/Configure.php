788a47ae5f9a1c65019739b85bf077c1f10ab613
Arkharis <arkharis@ya.ru>
2020-04-26 18:55:32 +0300

    Add support for 'Configurator' dir

--- src/Command/Configure.php
+++ src/Command/Configure.php
@@ -2,10 +2,29 @@
 
 namespace App\Command;
 
+/**
+ * Set configuration for a development copy
+ */
 class Configure extends Command
 {
+    /**
+     * Run command
+     *
+     * @return void
+     */
     public static function run()
     {
+        $confDir = dirname(__DIR__) . DIRECTORY_SEPARATOR . 'Configurator';
+        $presetName = self::$args->getArgument('preset');
+
+        $confClassFile = $confDir . DIRECTORY_SEPARATOR . ucfirst($presetName) . '.php';
+
+        if (!is_file($confClassFile)) {
+            throw new \Exception("Configuration settings file '{$confClassFile}' not found.");
+        }
+
+        require $confClassFile;
+
         echo 'Configuration setting complete', PHP_EOL;
     }
 }

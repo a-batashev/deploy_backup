62cbb94900b0970124aa1349830162a79c02cf2f
Arkharis <arkharis@ya.ru>
2020-05-11 15:21:47 +0300

    Edited Deploy messages

--- src/Command/Deploy.php
+++ src/Command/Deploy.php
@@ -6,11 +6,15 @@ class Deploy extends Command
 {
     public function run()
     {
+        $preset = self::$args->getArgument('preset');
+
+        echo "Deploy of '{$preset}' started", PHP_EOL;
+
         Clean::run();
         Download::run();
         Database::run();
         Configure::run();
 
-        echo 'Deploy is successfull';
+        echo "Deploy of '{$preset}' is successfull", PHP_EOL;
     }
 }

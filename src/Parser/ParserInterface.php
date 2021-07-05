e0f9a7f3da19aed5da4762da9c14f4c1d780e147
Arkharis <arkharis@ya.ru>
2021-07-05 12:24:54 +0300

    Add minor changes of ParserInterface

--- src/Parser/ParserInterface.php
+++ src/Parser/ParserInterface.php
@@ -4,5 +4,10 @@ namespace App\Parser;
 
 interface ParserInterface
 {
+    /**
+     * Undocumented function
+     *
+     * @param string $file Path to a file
+     */
     public static function parseFile($file);
 }

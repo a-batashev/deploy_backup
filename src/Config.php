04a1278a21de200366718166d64d0e8dc1db3865
a-batashev <AA.Batashev@npo-nauka.ru>
2023-12-18 16:26:15 +0300

    Update Config class

--- src/Config.php
+++ src/Config.php
@@ -3,51 +3,30 @@
 namespace App;
 
 use App\Parser;
+use App\Parser\ParserInterface;
+use Symfony\Component\Filesystem\Filesystem;
 
 /**
  * Stores settings from configuration file
  */
 class Config
 {
-    /**
-     * Configuration options
-     *
-     * @var array
-     */
+    /** @var Config Instance of Config */
+    protected static $instance;
+
+    /** @var string Current preset */
+    protected static $preset = '';
+
+    /** @var string Config filepath */
+    protected static $configFile = '';
+
+    /** @var array Configuration options */
     protected static $config = [];
 
     /**
-     * Instance of Config
-     *
-     * @var Config
-     */
-    protected static $instance;
-
-    /**
-     * Current preset
-     *
-     * @var string
-     */
-    protected static $preset;
-
-    /**
-     * Construct is hidden for singleton
+     * Disables constructor for singleton
      */
     private function __construct()
-    {
-    }
-
-    /**
-     * Disables cloning for singleton
-     */
-    private function __clone()
-    {
-    }
-
-    /**
-     * Disables unserialize for singleton
-     */
-    private function __wakeup()
     {
     }
 
@@ -56,45 +35,78 @@ class Config
      *
      * @return Config
      */
-    public static function getInstance(): self
+    public static function getInstance(): static
     {
-        if (is_null(self::$instance)) {
-            self::$instance = new self();
+        if (is_null(static::$instance)) {
+            static::$instance = new static();
         }
 
-        return self::$instance;
+        return static::$instance;
     }
 
     /**
      * Sets current preset
      *
      * @param string $presetName
-     * @return void
+     * @return Config
      */
-    public function setPreset(string $presetName)
+    public function setPreset(string $presetName): static
     {
-        self::$preset = $presetName;
+        static::$preset = $presetName;
+
+        return static::$instance;
+    }
+
+    /**
+     * Gets current preset
+     *
+     * @return string
+     */
+    public function getPreset(): string
+    {
+        return static::$preset;
+    }
+
+    /**
+     * Sets config file
+     *
+     * @param string $configFile
+     * @return Config
+     */
+    public function setConfig(string $configFile): static
+    {
+        static::$configFile = (new Filesystem())->isAbsolutePath($configFile)
+            ? $configFile
+            : dirname(__DIR__) . DIRECTORY_SEPARATOR . $configFile;
+
+        return static::$instance;
     }
 
     /**
      * Parses configuration file
      *
-     * @param string $file
-     * @return void
+     * @param string $file Absolute path to the configuration file
+     * @return array Parameters from the configuration file
      */
-    public function parse(string $file)
+    public function parse(string $file = null): array
     {
-        self::checkConfigFile($file);
+        $file = $file ?? static::$configFile;
 
-        $type = self::getType($file);
+        static::checkConfigFile($file);
 
-        $parser = self::chooseParser($type);
+        $type = static::getType($file);
 
-        self::$config = $parser::parseFile($file);
+        $parser = static::chooseParser($type);
 
-        self::checkSitePath();
+        static::$config = $parser::parseFile($file);
 
-        return self::$config;
+        // static::checkSitePath();
+
+        if (static::$preset) {
+            return static::$instance->getConfigByPreset();
+        }
+
+        return static::$config;
     }
 
     /**
@@ -105,9 +117,9 @@ class Config
      */
     public function getConfigByPreset(string $preset = null)
     {
-        $preset = $preset ?? self::$preset;
+        $preset = $preset ?? static::$preset;
 
-        $config = self::$config;
+        $config = static::$config;
 
         if (!isset($config['presets'][$preset])) {
             throw new \Exception("Preset '{$preset}' not found.");
@@ -120,6 +132,7 @@ class Config
      * Check configuration file
      *
      * @param string $file
+     * @throws \Exception
      * @return void
      */
     protected static function checkConfigFile(string $file)
@@ -140,7 +153,7 @@ class Config
      */
     protected static function checkSitePath()
     {
-        $config = self::$instance->getConfigByPreset();
+        $config = static::$instance->getConfigByPreset();
 
         if (!isset($config['sitePath'])) {
             throw new \Exception("Configuration option 'sitePath' is empty.");
@@ -155,9 +168,10 @@ class Config
      * Parser dispatching
      *
      * @param string $type
-     * @return void
+     * @throws \Exception
+     * @return ParserInterface
      */
-    protected static function chooseParser(string $type): Parser\ParserInterface
+    protected static function chooseParser(string $type): ParserInterface
     {
         switch ($type) {
             case 'yaml':
